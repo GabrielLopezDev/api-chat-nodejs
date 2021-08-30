8c0e3c32b0f846ae29028df7a7d31a7dc849716e
Gabriel López <gabriellopez.dev@gmail.com>
2021-08-30 16:51:44 -0400

    [add] añadir middlewares y refactorizar rutas

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
+
+const router = require('./network/routes');
 
 const app = express();
 
-app.use('/', (req, res) => {
-	res.send('Funciona!');
-});
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+router(app);
 
 app.listen(3000, () => {
-	console.log('La app se esta ejecutando en el puerto 3000...');
+	console.log("La app se esta ejecutando en el puerto 3000...");
 });
