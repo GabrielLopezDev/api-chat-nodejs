18434a9848689a0253482c01bc3b9a60edf34bde
Gabriel López <gabriellopez.dev@gmail.com>
2021-08-31 10:53:47 -0400

    [add] Mockup para gestionar la DB

---
+++ components/message/store.js
@@ -0,0 +1,15 @@
+const list = [];
+
+const addMessage = (message) => {
+	list.push(message);
+}
+
+const getMessage = () => {
+	return list;
+}
+
+
+module.exports = {
+	add: addMessage,
+	list: getMessage,
+}
