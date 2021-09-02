c95ba29db0baff5990fe0ff0890074bebe6fc6df
Gabriel López <gabriellopez.dev@gmail.com>
2021-09-02 14:37:12 -0400

    [fix] reparar error en el modelo Chat

--- components/chat/model.js
+++ components/chat/model.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const chatSchema = new Schema({
-	users: [
-		type: Schema.Types.ObjectId,
+	users: {
+		type: [Schema.Types.ObjectId],
 		ref: 'User',
-	]
+	}
 });
 
 const model = mongoose.model('Chat', chatSchema);
