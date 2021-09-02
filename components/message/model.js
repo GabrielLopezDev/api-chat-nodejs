08355a76a3df2c7e99ae8ecb96dfdbc4c8ff0673
Gabriel López <gabriellopez.dev@gmail.com>
2021-09-02 14:39:32 -0400

    [add] agregar campo chat al modelo message

--- components/message/model.js
+++ components/message/model.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
+	chat: {
+		type: Schema.Types.ObjectId,
+		ref: 'Chat',
+	},
 	user: {
 		type: Schema.Types.ObjectId,
 		ref: 'User',
