af71848aa1bacea5dd8267b087cee79638830dc3
Gabriel López <gabriellopez.dev@gmail.com>
2021-09-02 10:06:08 -0400

    [add] componente chat

--- network/routes.js
+++ network/routes.js
@@ -1,9 +1,11 @@
 const message = require('../components/message/routes');
 const user = require('../components/user/routes');
+const chat = require('../components/chat/routes');
 
 const routes = (server) => {
 	server.use('/message', message);
 	server.use('/user', user);
+	server.use('/chat', chat);
 }
 
 module.exports = routes;
