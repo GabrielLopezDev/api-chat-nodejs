8c8e8b0132b2874ae1567b543434ddf52fbd87d6
Gabriel López <gabriellopez.dev@gmail.com>
2021-09-03 10:37:27 -0400

    [add] agregar cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 
 const db = require('./db');
 const router = require('./network/routes');
@@ -11,6 +12,7 @@ db.connect(uri, options);
 const app = express();
 const server = require('http').Server(app);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/app', express.static('public'));
